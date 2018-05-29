3e6b7f130981fb62ff015c3dd61645cc0b4f44e0
nahdazahra <nahda15@mhs.if.its.ac.id>
2018-05-29 14:40:16 +0700

    client

--- FP/client.py
+++ FP/client.py
@@ -5,11 +5,42 @@ import sys
 def main():
 	uri = 'PYRO:middleware@127.0.0.1:9001'
 	middleware = Pyro4.Proxy(uri)
-	arg = sys.argv
-	nama = arg[1]
-	print str(nama)
+	cmd = middleware.[fungsi waktu ngambil command]
+	cwd = 'folder'
+	args = []
+	while True:
+		print ('cli >> '+ cwd + ' ')
+		command = raw_input()
+		args.extend(command.split())
 
-	middleware.setData(nama)
+		if args[0] in cmd:
+			if args[0] == 'cd':
+				cwd = middleware.args(args, cwd)
+
+			if args[0] == 'ls':
+				cwd = middleware.args(args, cwd)
+				if(isinstance(results, list)):
+					for result in results:
+						print(result)
+				else:
+					print(results)
+					
+			if args[0] == 'rm' or args[0] == 'touch':
+				cwd = middleware.args(args, cwd)
+
+			if args[0] == 'cp' or args[0] == 'mv':
+				cwd = middleware.args(args, cwd)
+
+		elif args[0] == '':
+			continue
+		
+		elif args[0] == 'quit':
+			break
+
+		else:
+			print ('server >> command \''+args[0]+'\' not found')
+
+
 
 if __name__ == '__main__':
 	main()
